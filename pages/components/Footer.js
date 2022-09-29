51cf6d38cf08bf6ab92c1a11afd14ea79caebcaf
sghere <gshubham8@gmail.com>
2022-09-29 23:36:34 +0530

    UI part 1

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import {
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+  AiOutlineTwitter,
+  AiOutlineYoutube,
+} from 'react-icons/ai';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="container">
+      <hr />
+      <div class="FooterContainer pt-5 pb-5 text-center">
+        Footer
+        <p>need help? email: gshubham8@gmail.com</p>
+        <p>
+          <AiOutlineInstagram />
+          <AiOutlineFacebook />
+          <AiOutlineTwitter />
+          <AiOutlineYoutube />
+        </p>
+        <small>Copyright © 2022 Shubham Gaikwad</small>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- pages/components/HeroSection.jsx
+++ pages/components/HeroSection.jsx
@@ -5,13 +5,13 @@ const HeroSection = () => {
     <>
       <div className="container">
         <div className="HeroSection">
-          <div className="grid fr2 pt-5">
+          <div className="grid fr2">
             <div className="grid place-center">
               <h1 className="text-center">
                 decorate yourself with
                 <span className="gradient"> timeless things</span>
                 <br />
-                <button className="btn">get started</button>
+                <button className="btn">start shopping</button>
               </h1>
             </div>
             <div className="grid place-center">
