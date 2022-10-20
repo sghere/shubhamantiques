d739160426cc7fec63b0621ab333b3a167766b4f
sghere <gshubham8@gmail.com>
2022-10-20 23:58:10 +0530

    Trying Products look

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,29 +1,24 @@
-import Image from "next/image";
-import React from "react";
-import img from "../public/images/abc.jpg";
+import Image from 'next/image';
+import React from 'react';
+import img from '../public/images/abc.jpg';
+import styles from './ProductCard.module.css';
 
-const ProductCard = () => {
+const ProductCard = ({
+  img,
+  ProductName,
+  ProductDescription,
+  ProductPrice,
+}) => {
   return (
-    <div className="Card">
-      <Image src={img} layout="responsive" width={300} height={200} />
-      <div className="CardBody">
-        <div className="CardHeader">
-          <h4>Product Name</h4>
-          <button className="btn">buy now</button>
+    <div className={styles.Card}>
+      <Image src={img} />
+      <div className={styles.CardBody}>
+        <div className={styles.CardHeader}>
+          <h2>{ProductName}</h2>
+          {/* <button className="btn">buy now</button> */}
+          <h2 className={styles.ProductPrice}>{ProductPrice}/-</h2>
         </div>
-
-        <p>
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters.It is a long established fact that a reader will be distracted
-          by the readable content of a page when looking at its layout. The
-          point of using Lorem Ipsum is that it has a more-or-less normal
-          distribution of letters. It is a long established fact that a reader
-          will be distracted by the readable content of a page when looking at
-          its layout. The point of using Lorem Ipsum is that it has a
-          more-or-less normal distribution of letters.
-        </p>
+        <p>{ProductDescription}</p>
       </div>
     </div>
   );
--- components/ProductsContainer.jsx
+++ components/ProductsContainer.jsx
@@ -1,13 +1,38 @@
-import React from "react";
-import ProductCard from "./ProductCard";
+import React, { useState, useEffect, useTransition } from 'react';
+import ProductCard from './ProductCard';
+import img from '../public/images/abc.jpg';
 
 const ProductsContainer = () => {
+  const [Data, setData] = useState([]);
+  const [IsLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      fetch('https://dummyjson.com/products')
+        .then((res) => res.json())
+        .then((result) => {
+          setIsLoading(false);
+          console.log(result.products);
+          setData(result.products);
+        });
+
+      console.log('5 sec hua');
+    }, 15000);
+  }, []);
+
+  const DisplayProducts = Data.map((e) => (
+    <ProductCard
+      id={e.id}
+      img={img}
+      ProductName={e.title}
+      ProductDescription={e.description}
+      ProductPrice={'$' + e.price}
+    />
+  ));
+
   return (
     <div className="ProductsContainer">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      {IsLoading ? 'Loading....' : DisplayProducts}
     </div>
   );
 };
