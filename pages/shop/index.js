513efc36799cf2bd04dfc966e7a5bcc50c1d33b8
Shubham Gaikwad <99805248+sghere@users.noreply.github.com>
2023-03-05 02:59:11 +0530

    abc

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -4,7 +4,7 @@ import React from "react";
 import Layout from "../../components/Layout";
 import ProductsContainer from "../../components/ProductsContainer";
 
-const index = ({ Data }) => {
+const index = ({ Data = [] }) => {
   return (
     <>
       <Head>
